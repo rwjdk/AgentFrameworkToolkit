221ac000401a47c64b912580243050467ee551c8
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2026-02-21 09:33:11 +0100

    WIP

--- development/Sandbox/Providers/AzureOpenAI.cs
+++ development/Sandbox/Providers/AzureOpenAI.cs
@@ -1,67 +1,53 @@
 using AgentFrameworkToolkit;
 using AgentFrameworkToolkit.AzureOpenAI;
 using AgentFrameworkToolkit.OpenAI;
-using AgentFrameworkToolkit.Tools;
-using AgentFrameworkToolkit.Tools.Common;
 using Microsoft.Agents.AI;
-using Microsoft.Extensions.AI;
-using OpenAI.Chat;
-using OpenAI.Responses;
 using Secrets;
-
-#pragma warning disable OPENAI001
 
 namespace Sandbox.Providers;
 
 public static class AzureOpenAI
 {
-    [AITool]
-    static string GetWeather()
-    {
-        return "Sunny af 30 degrees";
-    }
-
     public static async Task RunAsync()
     {
         Secrets.Secrets secrets = SecretsManager.GetSecrets();
 
-        //Create your AgentFactory (using a connection object for more options)
         AzureOpenAIAgentFactory factory = new AzureOpenAIAgentFactory(new AzureOpenAIConnection
         {
             Endpoint = secrets.AzureOpenAiEndpoint,
             ApiKey = secrets.AzureOpenAiKey,
         });
 
-        AzureOpenAIAgent aiAgent = factory.CreateAgent(new AgentOptions
+        AzureOpenAIAgent agent = factory.CreateAgent(new AgentOptions
         {
             Model = "gpt-4.1-mini",
             RawToolCallDetails = Console.WriteLine
         });
 
-        AgentResponse<int> runAsync = await aiAgent.RunAsync<int>("What is 2+2");
-
-        List<AITool> tools = [];
-        tools.AddRange(EmailTools.All(new EmailToolsOptions
-        {
-            Host = "send.one.com",
-            FromAddress = "mail@rwj.dk",
-            ConfineSendingToTheseDomains = ["gmail.com"],
-            FromDisplayName = "RWJ",
-            Port = 587,
-            UseSecureConnection = true,
-            Username = secrets.EmailUsername,
-            Password = secrets.EmailPassword
-        }));
-
-        AzureOpenAIAgent agent = factory.CreateAgent(new AgentOptions
+        AIAgent agent2A = factory.CreateAgent(new AgentOptions
         {
             Model = "gpt-4.1-mini",
-            Tools = tools,
-            RawToolCallDetails = Console.WriteLine 
         });
 
+        AIAgent agent2B = factory.CreateAgent(new AgentOptions
+        {
+            Model = "gpt-4.1-mini",
+            RawToolCallDetails = Console.WriteLine
+        });
+        string question = "What is 2+2";
 
-        AgentResponse agentResponse = await agent.RunAsync("send a mail to rwj@relewise.com with a poem about ducks");
+        AgentResponse<MathResult> response1 = await agent.RunAsync<MathResult>(question);
+
+        AgentResponse<MathResult> response2A = await agent2A.RunAsync<MathResult>(question); //Fail do to Microsoft Bug
+        
+        AgentResponse<MathResult> response2B = await agent2B.RunAsync<MathResult>(question); //Fail do to Microsoft Bug
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        AgentResponse<MathResult> response2BFix1 = await AgentFrameworkToolkit.AIAgentExtensions.RunAsync<MathResult>(agent2B, question);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        AgentResponse<MathResult> response3Fix2 = await ((Agent)agent2B).RunAsync<MathResult>(question);
+
     }
 
     public class MathResult
